555b752857d6166649982025e90aa24c17bcd59d
abolfazl930 <a.abolfazl930@gmail.com>
2020-10-14 23:45:46 +0330

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState([]);
-  useEffect(e => {
+  useEffect((e) => {
     axios
       .get("https://api.spacexdata.com/v3/launches")
       .then(function (response) {
