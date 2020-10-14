ee083319eeaf9860f60aa9960c7da39724a118dc
abolfazl930 <a.abolfazl930@gmail.com>
2020-10-14 22:34:03 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [data,setData]= useState([]);
+  useEffect(()=>{axios.get('https://api.spacexdata.com/v3/launches')
+  .then(function (response) {
+    console.log("response",response);
+  })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
